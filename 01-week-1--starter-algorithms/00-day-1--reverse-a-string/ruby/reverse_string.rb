a187f563e6503891f6a7acc9aaa499e96f08a6eb
Leigha De La Rosa <cdversiontwo@gmail.com>
2021-08-12 15:14:41 -0700

    solve ruby 01-01

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/ruby/reverse_string.rb
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/ruby/reverse_string.rb
@@ -1,15 +1,27 @@
 def reverse_string(str)
   # type your code in here
+  reversed_string = ""
+
+  str.chars.each do |char|
+    reversed_string = char + reversed_string
+  end
+
+  reversed_string
 end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'ih'"
   puts "=>", reverse_string('hi')
 
-  puts
+  puts 
 
   puts "Expecting: 'ybabtac'"
   puts "=>", reverse_string('catbaby')
+
+  puts 
+
+  puts "Expecting: 'kaetseseehc'"
+  puts "=>", reverse_string('cheesesteak')
 
   # Don't forget to add your own!
 end
