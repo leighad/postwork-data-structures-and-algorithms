86796a846a71b85aef43fcfa108536e9080d17d8
Leigha De La Rosa <cdversiontwo@gmail.com>
2021-08-16 19:19:24 -0700

    sovle ruby 01-02

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/ruby/find_first_duplicate.rb
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/ruby/find_first_duplicate.rb
@@ -1,5 +1,14 @@
 def find_first_duplicate(arr)
   # type your code in here
+  counter_hash = Hash.new(0)
+
+  arr.each do |el|
+    counter_hash[el] = counter_hash[el] += 1
+
+    return el if counter_hash[el] > 1
+  end
+
+  return -1
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -12,7 +21,13 @@ if __FILE__ == $PROGRAM_NAME
   puts "=>", find_first_duplicate([1, 2, 3, 4])
 
   # Don't forget to add your own!
+  puts
+
+  puts "Expecting: 5"
+  puts "=>", find_first_duplicate([1, 5, 2, 3, 5, 4, 1, 3])
 end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+
+# Very helpful walkthrough at https://www.youtube.com/watch?v=ix-cOz8YRGs
